b3176ef505b2f89c73daf00cd9c1ce223b0b5fd6
Chihoshka <85583329+Chihoshka@users.noreply.github.com>
2021-06-08 22:08:11 +0300

    Add files via upload

---
+++ page/binaryCalculator.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let btns = document.querySelectorAll("#btnSum, #btnSub, #btnMul, #btnDiv, #btn0, #btn1, #btnClr");
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].onclick = function () {
+        if (this.innerHTML === "C") {
+            document.getElementById("res").innerHTML = ""; 
+        } else {
+            document.getElementById("res").innerHTML += this.innerHTML;
+        }
+    }
+}
+
+document.getElementById("btnEql").onclick = function () {
+    let arr = document.getElementById("res").innerHTML.match(/([01]+)([\+\-\*\/])([01]+)/);
+    let n = parseInt(arr[1], 2) + arr [2] + parseInt(arr[3], 2); // 11+1 3+1
+    document.getElementById("res").innerHTML = Math.floor(eval(n)).toString(2);
+}
